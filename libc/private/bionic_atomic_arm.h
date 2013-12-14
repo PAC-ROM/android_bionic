a480ea2a0a23dc3bc867c9fe5c6b23ccc7153a8b
Elliott Hughes <enh@google.com>
2013-12-13 16:54:16 -0800

    Clean up the pthread-only atomic stuff a little.
    It looks like we can probably just use the generic GCC stuff instead;
    the generated code looks pretty similar. We should come back to that.
    
    These routines are only used by the pthread implementation, and
    __bionic_atomic_inc isn't used, so we can remove it.
    
    Conflicts:
    	libc/private/bionic_atomic_arm.h
    
    Change-Id: I8b5b8cb30a1b159f0e85c3675aee06ddef39b429

--- libc/private/bionic_atomic_arm.h
+++ libc/private/bionic_atomic_arm.h
@@ -16,51 +16,14 @@
 #ifndef BIONIC_ATOMIC_ARM_H
 #define BIONIC_ATOMIC_ARM_H
 
-__ATOMIC_INLINE__ void __bionic_memory_barrier() {
+__ATOMIC_INLINE__ void __bionic_memory_barrier(void) {
 #if defined(ANDROID_SMP) && ANDROID_SMP == 1
-  __asm__ __volatile__ ( "dmb ish" : : : "memory" );
+  __asm__ __volatile__ ( "dmb" : : : "memory" );
 #else
   /* A simple compiler barrier. */
   __asm__ __volatile__ ( "" : : : "memory" );
 #endif
-
-/* Define a full memory barrier, this is only needed if we build the
- * platform for a multi-core device. For the record, using a 'dmb'
- * instruction on a Nexus One device can take up to 180 ns even if
- * it is completely un-necessary on this device.
- *
- * NOTE: This is where the platform and NDK headers atomic headers are
- *        going to diverge. With the NDK, we don't know if the generated
- *        code is going to run on a single or multi-core device, so we
- *        need to be cautious.
- *
- *        I.e. on single-core devices, the helper immediately returns,
- *        on multi-core devices, it uses "dmb" or any other means to
- *        perform a full-memory barrier.
- *
- * There are three cases to consider for the platform:
- *
- *    - multi-core ARMv7-A       => use the 'dmb' hardware instruction
- *    - multi-core ARMv6         => use the coprocessor
- *    - single core ARMv6+       => do not use any hardware barrier
- */
-#if defined(ANDROID_SMP) && ANDROID_SMP == 1
-
-/* For ARMv7-A, we can use the 'dmb' instruction directly */
-__ATOMIC_INLINE__ void __bionic_memory_barrier(void) {
-    /* Note: we always build in ARM or Thumb-2 on ARMv7-A, so don't
-     * bother with __ATOMIC_SWITCH_TO_ARM */
-    __asm__ __volatile__ ( "dmb" : : : "memory" );
 }
-
-#else /* !ANDROID_SMP */
-
-__ATOMIC_INLINE__ void __bionic_memory_barrier(void) {
-    /* A simple compiler barrier */
-    __asm__ __volatile__ ( "" : : : "memory" );
-}
-
-#endif /* !ANDROID_SMP */
 
 /* Compare-and-swap, without any explicit barriers. Note that this functions
  * returns 0 on success, and 1 on failure. The opposite convention is typically
